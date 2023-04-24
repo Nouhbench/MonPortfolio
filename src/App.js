6207d5efc75df53d43a6a010b15b6bcfe0ef64b7
Nouhbench <128232274+Nouhbench@users.noreply.github.com>
2023-04-24 15:25:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Accueil from "./components/Acceuil";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Formations from "./components/Formations";
+import Experience from "./components/Experience";
+import Projets from "./components/Projets";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ backgroundColor: "white" }}>
+      <BrowserRouter>
+        <Header />
+        <div>
+            <Routes>
+              <Route path="/*" element={<Accueil />}></Route>
+              <Route path="/formation/*" element={<Formations />}></Route>
+              <Route path="/experience/*" element={<Experience />}></Route>
+              <Route path="/projet/*" element={<Projets />}></Route>
+            </Routes>
+          
+        </div>
+        <Footer/>
+        </BrowserRouter>
+
+      </div>
+
     </div>
   );
 }
